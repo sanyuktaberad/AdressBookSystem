5635da8732efc93214f6b5117867da1e27107d20
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-07 21:26:20 +0530

    Refactor: Code for adding a new contact is added.

--- src/com/bridgelabz/AdessbookMain.java
+++ src/com/bridgelabz/AdessbookMain.java
@@ -1,5 +1,73 @@
 package com.bridgelabz;
 
-public class AdessbookMain {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class AdessbookMain {
+	private ArrayList<Contacts> ContactList;
+
+    public  AdessbookMain() {
+        ContactList = new ArrayList<Contacts>();
+    }
+
+    public void addContact(Contacts createPerson) {
+        System.out.println("Adding a New Contact ");
+        ContactList.add(createPerson);
+    }
+
+    public Contacts getContactInput() {
+    	
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter the Address: ");
+        String address = sc.nextLine();
+        System.out.println("Enter the City: ");
+        String city = sc.nextLine();
+        System.out.println("Enter the State: ");
+        String state = sc.nextLine();
+        System.out.println("Enter the Zip: ");
+        int zip = sc.nextInt();
+        System.out.println("Enter the Phone No: ");
+        String phoneNumber = sc.nextLine();
+        sc.nextLine();
+        System.out.println("Enter the Email: ");
+        String email = sc.nextLine();
+
+        Contacts contact = new Contacts(firstName, lastName, address, city, state, email, zip, phoneNumber);
+        return contact;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Address Book System");
+        int ch = 0;
+        Scanner sc = new Scanner(System.in);
+        AdessbookMain AD = new AdessbookMain();
+
+        while (true) {
+            System.out.println("1.Adding Contact \n2.Exit");
+            System.out.println("Enter a Your Choice: ");
+            ch = sc.nextInt();
+
+            switch (ch) {
+                case 1:
+                    Contacts contact = AD.getContactInput();
+                    AD.addContact(contact);
+
+                    System.out.println("Contact Added Successfully ");
+                    break;
+
+                case 2:
+                    System.out.println("Thank You We are Exiting");
+                    return;
+
+                default:
+                    System.out.println("Sorry You are Type Wrong Choice");
+            }
+        }
+    }
 }
+
--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -1,5 +1,83 @@
 package com.bridgelabz;
 
 public class Contacts {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private int phonenumber;
+	private String email;
 
+
+
+	public Contacts(String firstName2, String lastName2, String address2, String city2, String state2, String email2,
+			int zip2, String phoneNumber2) {
+	
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
